c67f2a6fb161d064e24992dd660738bb7416385f
KhNishad <kkhnishad@gmail.com>
2026-05-06 23:55:22 +0600

    category curd

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,23 +1,38 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Query,
+  Param,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/createCategory.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('category')
 export class CategoryController {
-    constructor(private readonly categoryService: CategoryService) { }
+  constructor(private readonly categoryService: CategoryService) {}
 
-    @Post('/create')
-    createCategory(@Body() body: CreateCategoryDto) {
-        return this.categoryService.createCategory(body)
-    }
+  @Post('/create')
+  createCategory(@Body() body: CreateCategoryDto) {
+    return this.categoryService.createCategory(body);
+  }
 
-    @Get('/get-list')
-    getCategoryList() {
-        return this.categoryService.getList();
-    }
+  @Get('/get-list')
+  @UseGuards(AuthGuard('jwt'))
+  getCategoryList(
+    @Query() query: { page: number; limit: number; search: string },
+  ) {
+    return this.categoryService.getCategoryList(query);
+  }
 
+  @Get('/single-category/:id')
+  @UseGuards(AuthGuard('jwt'))
+  getSingleCategory(@Param('id') id: string) {
+    return this.categoryService.getSingleCategory(id);
+  }
 
-
-
-
+  
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,29 +1,83 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from './schemas/createCategorySchema';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreateCategoryDto } from './dto/createCategory.dto';
 import { generateSlug } from 'src/utils/slug.utlis';
 
 @Injectable()
 export class CategoryService {
-    constructor(@InjectModel(Category.name)
-    private categoryModel: Model<CategoryDocument>,) { }
+  constructor(
+    @InjectModel(Category.name)
+    private categoryModel: Model<CategoryDocument>,
+  ) {}
 
-    async createCategory(body: CreateCategoryDto) {
+  async createCategory(body: CreateCategoryDto) {
+    const slug = generateSlug(body.name);
 
-        const slug = generateSlug(body.name);
-
-        const existingCategory = await this.categoryModel.findOne({ slug });
-        if (existingCategory) {
-            throw new ConflictException(`Category "${body.name}" already exists`);
-        }
-
-        const category = await this.categoryModel.create({ ...body, slug });
-        return {
-            message: 'Category created successfully',
-            data: category,
-            success: true,
-        };
+    const existingCategory = await this.categoryModel.findOne({ slug });
+    if (existingCategory) {
+      throw new ConflictException(`Category "${body.name}" already exists`);
     }
+
+    const category = await this.categoryModel.create({ ...body, slug });
+    return {
+      message: 'Category created successfully',
+      data: category,
+      success: true,
+    };
+  }
+
+  async getCategoryList(query: {
+    page: number;
+    limit: number;
+    search: string;
+  }) {
+    let page = query.page || 1;
+    let limit = query.limit || 10;
+    let search = query.search || '';
+    const skip = (page - 1) * limit;
+
+    const filter = search
+      ? {
+          $or: [
+            { name: { $regex: search, $options: 'i' } },
+            { slug: { $regex: search, $options: 'i' } },
+          ],
+        }
+      : {};
+
+    const categories = await this.categoryModel
+      .find(filter)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+    return {
+      message: 'Categories fetched successfully',
+      data: categories,
+      success: true,
+    };
+  }
+
+  async getSingleCategory(id: string) {
+    
+    if(!isValidObjectId(id)){
+      throw new BadRequestException('Invalid Category ID');
+    }
+
+    const category = await this.categoryModel.findById({ _id: id });
+    if (!category) {
+      throw new NotFoundException('Category Not Found');
+    }
+    return {
+      message: 'Category fetched successfully',
+      data: category,
+      success: true,
+    };
+  }
 }
